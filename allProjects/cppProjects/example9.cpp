f2a4a207991972f6b2287c3af7f5275f69d4bca7
loupeters <peterso@purdue.edu>
2015-08-11 14:28:09 -0500

    new examples

--- allProjects/cppProjects/example9.cpp
+++ allProjects/cppProjects/example9.cpp
@@ -5,8 +5,10 @@
 using namespace std;
 
 int main() {
-  char first_name[30], last_name[30]; int age;
+  char first_name[30], last_name[30]; 
+  int age;
   char file_name[20];
+  
   // Collect the data.
   cout << "Enter First Name: "; cin >> first_name;
   cout << "Enter Last Name: "; cin >> last_name;
@@ -15,6 +17,24 @@ int main() {
 
   // Create an ofstream called People, open the stream for output.
   ofstream People(file_name, ios::out);
+  
   // Write the output to the stream.
-  People << first_name << endl << last_name << endl << age << endl; return 0;
-} 
+  People << first_name << endl << last_name << endl << age << endl; 
+  
+  //close the stream
+  People.close();
+
+  // Create an ifstream to read the file.
+  ifstream People_in(file_name);
+  People_in >> first_name >> last_name >> age;
+
+  cout << endl << "First Name: " << first_name;
+  cout << endl << "Last Name:  " << last_name;
+  cout << endl << "Enter Age:  " << age;
+  cout << endl;
+
+  // Close the stream.
+  People_in.close();
+
+  return 0;
+}
--- allProjects/cppProjects/random.cpp
+++ allProjects/cppProjects/random.cpp
@@ -1,4 +1,5 @@
-// Description: Illustrates how to generate a random number in C++
+// Description: Illustrates how to generate a random number in C++ within A guessing game where the player guesses
+// the secret number.
 
 #include <iostream>
 #include <time.h>
@@ -13,17 +14,18 @@ int main () {
   // Generate random number between 1 and 100
   random_number = rand() % 100 + 1;
 
-
+  do{
   cout << "Guess the number (1 to 100) ";
   if(!(cin >> guess)){
   		cout << "only numeric numbers allowed!" << endl;
   } else {
   	if (random_number < guess) cout << "The secret number is lower than " << guess << endl;
   	else if (random_number > guess) cout << "The secret number is greater than " << guess << endl;
-  	else cout << "You guessed it!" << random_number << endl;
+  	else cout << "You guessed it! " << random_number << endl;
   }
+  }while(guess != random_number); 
 
-  cout << "random: " << random_number << endl;
+  cout << "CONGRATS!! THE SECRET NUMBER IS: " << random_number << endl;
   return 0;
 }
  	
