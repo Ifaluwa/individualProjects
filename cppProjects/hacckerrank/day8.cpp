ba7134c2f0794947099215d4fdf227a48599bafd
Laolu Peters <peterso@purdue.edu>
2016-07-11 23:09:00 -0700

    hackerrank code-day 8

---
+++ cppProjects/hacckerrank/day8.cpp
@@ -0,0 +1,44 @@
+//Author: Laolu Peters
+// Hackerrank warmups, Summer '16'
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int N;
+    cin >> N;
+    
+    
+    //defining dictionary
+    map<string, long int> dict;
+    
+    //Filling dictionary
+    for(int i = 0; i < N; i++){
+        string key;
+        long int value;
+        cin >> key;
+        cin >> value;
+        dict[key] = value;
+    }
+    
+    //checking entries
+    for(int i = 0; i < N; i++){
+        string key;
+        cin >> key;
+        
+        if (dict.find(key) != dict.end()){
+            cout << key << "=" << dict[key] << endl;
+        } else {
+            cout << "Not found" << endl;
+        }
+    }
+
+    return 0;
+}
